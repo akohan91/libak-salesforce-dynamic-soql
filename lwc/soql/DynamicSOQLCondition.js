779376f266a0dc3cb955a28a2aaa8b574aeec8e5
akohan91 <akohan91@gmail.com>
2023-02-02 22:48:06 +0100

    Libak_0014/01_Add_logic_to_process_incoming_values_to_DynamicSOQLCondition.js

--- lwc/soql/DynamicSOQLCondition.js
+++ lwc/soql/DynamicSOQLCondition.js
@@ -2,26 +2,64 @@
  * @author Andrew Kohanovskij <akohan91@gmail.com>
  */
 export class DynamicSOQLCondition {
-	// String
+	/**
+	 * @type DynamicSOQLFunction
+	 */
+	function;
+	/**
+	 * @description Sobject field name. Also it could be a path like Account.Name
+	 * @type String
+	 */
 	fieldName;
-	// String
+	/**
+	 * @description SOQL confition operator
+	 * @type String
+	 */
 	operator;
-	// String
+	/**
+	 * @description Value of SOQL condition
+	 * @type String
+	 */
 	value;
-	// Boolean
-	isString;
+	/**
+	 * @description Array of values that is used for 'IN', 'NOT IN', 'INCLUDES', 'EXCLUDES' operators
+	 * @type Array
+	 */
+	values;
 
 	/**
 	 *
 	 * @param { String } fieldName
 	 * @param { String } operator
 	 * @param { Any } value
-	 * @param { Boolean } isString
 	 */
-	constructor(fieldName, operator, value, isString) {
-		this.fieldName = fieldName;
+	constructor(fieldName, operator, value) {
+		this._setFieldNameOrFunction(fieldName);
 		this.operator = operator;
-		this.value = value;
-		this.isString = isString === true;
+		this._setValue(value);
+	}
+
+	_setFieldNameOrFunction(fieldName) {
+		console.log('fieldName',fieldName);
+		console.log("typeof fieldName === 'object'",typeof fieldName === 'object');
+		if (typeof fieldName === 'object' && fieldName !== null) {
+			this.function = fieldName;
+			return;
+		} else {
+			this.fieldName = fieldName;
+		}
+	}
+
+	_setValue(value) {
+		if (value === null) {
+			this.value = 'null';
+			return;
+		} else if (Array.isArray(value)) {
+			this.values = value;
+			return;
+		} else {
+			this.value = value;
+			return;
+		}
 	}
 }
